59e2af9e45f6ed2f6ebb739b2929884bd8d51bc0
ben.sims <ben.sims@corelogic.co.uk>
2013-09-24 12:22:04 +0100

    adding parameters for branch and path, parameterise git svn commands

--- gitsvntrackbranch.sh
+++ gitsvntrackbranch.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+    echo "Usage: gitsvntrackbranch.sh <branch> <path>" 
+    exit
+fi
+echo "Adding branch http://svn.corelogic.local/svn/main/branches/$2/$1 to git svn tracking branches"
+read -p "Confirm this is correct (Yy)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+     echo "Adding new tracking branch"
+     git config --add svn-remote.dev-$1.url http://svn.corelogic.local/svn/main/branches/$2/$1
 
+     git config --add svn-remote.dev-$1.fetch :refs/remotes/git-svn-dev-$1
 
-git config --add svn-remote.dev-mosaic-sales.url http://svn.corelogic.local/svn/main/branches/mosaic-sales/mosaic-sales
-git config --add svn-remote.dev-mosaic-sales.fetch :refs/remotes/git-svn-dev-mosaic-sales
-
-git svn fetch dev-mosaic-sales
-git branch --track dev-mosaic-sales git-svn-dev-mosaic-sales
-git checkout dev-mosaic-sales
-git svn rebase
+     git svn fetch dev-$1
+     git branch --track dev-$1 git-svn-dev-$1
+     git checkout dev-$1
+     git svn rebase
+fi
