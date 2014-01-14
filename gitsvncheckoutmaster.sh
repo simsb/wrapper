d1c898d60c7cf0e900122f2ec525a92ae77ece3b
ben.sims <ben.sims@corelogic.co.uk>
2014-01-14 18:12:41 +0000

    no longer need the parameter

--- gitsvncheckoutmaster.sh
+++ gitsvncheckoutmaster.sh
@@ -3,21 +3,16 @@
 currentBranch=$(git branch | sed --quiet 's/* \(.*\)/\1/p')
 activeTrackingBranch="$(cat ~/.gitsvnactivetrackingbranch)";
 
-if [ -z "$1" ]
+
+
+
+ if [ -z "$activeTrackingBranch" ];
 then
-    echo "Usage: gitsvncheckoutmaster.sh <branch>"
-    exit
-fi
-
-
-
- if [ -z "$activeTrackingBranch" ] || [ "$activeTrackingBranch" != "$1" ];
-then
-    echo "Can only switch to currently active svn tracking branch which is $activeTrackingBranch."
+    echo "no active tracking branch - run gitsvnsetmaster.sh"
     exit
 else
     echo "Checking out currently active tracking branch which is $activeTrackingBranch"
-    git checkout $1
+    git checkout $activeTrackingBranch
 
     exit
 fi
